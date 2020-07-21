b64d60516796e12426d3c773a1d4026f3b92734e
Pistachu <ygomezma@gmail.com>
2020-07-21 21:11:41 +0200

    Maquetación casi terminada

---
+++ js/moledo.js
@@ -0,0 +1,22 @@
+let listaTareas = new Array(
+    {
+        id: 1,
+        titulo: 'Hacer la segunda práctica del bootcamp',
+        prioridad: 'urgente',
+    },
+    {
+        id: 2,
+        titulo: 'Estudiar javascript',
+        prioridad: 'diaria',
+    },
+    {
+        id: 3,
+        titulo: 'Poner la lavadora',
+        prioridad: 'semanal',
+    },
+    {
+        id: 4,
+        titulo: 'Planchar',
+        prioridad: 'mensual',
+    },
+);
